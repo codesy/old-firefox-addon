cc8878b7b729df2041fde6972e97127b64c0d1ba
groovecoder <luke.crouch@gmail.com>
2013-11-24 14:56:52 -0600

    Initial commit of Firefox extension

---
+++ README.md
@@ -0,0 +1,13 @@
+codesy.io Firefox Extension
+===========================
+
+The codesy.io Firefox Extension adds codesy.io information to open-source bugs
+you visit.
+
+Getting Started with Development
+--------------------------------
+
+1. Clone this repo
+2. [Install the Firefox Add-on SDK](https://addons.mozilla.org/developers/docs/sdk/latest/dev-guide/tutorials/installation.html) to get the SDK Virtual Environment
+3. Fiddle with the code
+4. Run `cfx run` to run an instance of Firefox with your code changes
---
+++ data/github.js
@@ -0,0 +1,8 @@
+// Send a request to codesy.io with the URL
+// Add a new widget under div.discussion-labels showing:
+// * Whether the bug has any bids in codesy
+// * An offer input, filled with user's existing bid if applicable
+// * An ask input, filled with user's existing bid if applicable
+$(".discussion-sidebar").append('<img src="' + self.options.codesyImgUrl + '"/><form><input type="text" placeholder="5.00" id="offer"/><br/><input type="text" placeholder="500.00" id="ask"/><br/><input type="submit"/>');
+
+$.getJSON('http://codesy.io/bids?url=' + window.location);

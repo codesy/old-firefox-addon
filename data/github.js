c3e055bef03d625d75618550588529b6d5e2bf6d
groovecoder <luke.crouch@gmail.com>
2013-12-03 21:57:37 -0600

    add <hr> above codesy widget <div>

--- data/github.js
+++ data/github.js
@@ -3,11 +3,14 @@
 // * Whether the bug has any bids in codesy
 // * An offer input, filled with user's existing bid if applicable
 // * An ask input, filled with user's existing bid if applicable
-var html = '<img src="' + self.options.codesyImgUrl + '"/>' +
+var html = '<hr/>' +
+    '<div id="codesy-widget">' +
+    '<img src="' + self.options.codesyImgUrl + '"/>' +
     '<form id="codesy" action="https://' + self.options.codesyDomain + '/bids" method="POST">' +
     '<input type="text" placeholder="offer amount" id="bid_offer" name="bid[offer]"/><br/>' +
     '<input type="text" placeholder="ask amount" id="bid_ask" name="bid[ask]"/><br/>' +
-    '<a class="button minibutton">Bid</a>';
+    '<a class="button minibutton">Bid</a>' +
+    '</div>';
 $(".discussion-sidebar").append(html);
 $form = $("form#codesy");
 $form.find("a.button").click(function(){
